972ade126ff7a616065a5ec7293ae10ba6c871e5
Andrew Low <Andrew_Low@ca.ibm.com>
2022-07-10 11:38:26 -0400

    Update level1.js

--- level1.js
+++ level1.js
@@ -2,17 +2,21 @@
 export async function main(ns) {
 	//
 	// starter script - target every node we can root
-	// and run an attack on n00dles
-	var port0 = [
-		"n00dles",
-		"foodnstuff",
-		"sigma-cosmetics",
-		"joesguns",
-		"hong-fang-tea",
-		"harakiri-sushi",
-		"nectar-net"]
+	// and run an attack on args[0]
+	var port1 = [
+		"zer0",
+		"max-hardware",
+		"CSEC",
+		"neo-net",
+		"iron-gym"]
 
-	for (var i = 0; i < port0.length; i++) {
-		await ns.exec("bust.js", "home", 1, "n00dles", port0[i])
+	ns.tprintf("Attacking " + ns.args[0] + ' with all port 1 servers')
+
+	for (var i = 0; i < port1.length; i++) {
+		// first kill if anything on the server
+		await ns.killall(port1[i], false)
+
+		// run bust on this server
+		await ns.exec("bust.js", "home", 1, ns.args[0], port1[i])
 	}
 }
